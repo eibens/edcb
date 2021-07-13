31f581947a372a84a4c61b44d198f30b687931c8
eibens <l.eibensteiner@gmail.com>
2021-07-13 19:31:30 +0200

    fix(cli): version resolution and validations

--- cli.ts
+++ cli.ts
@@ -6,15 +6,43 @@ import {
 } from "https://deno.land/std@0.101.0/fmt/colors.ts";
 
 if (import.meta.main) {
-  console.log(cyan("[edcb] downloading"));
-  const url = "https://deno.land/x/edcb@v0.4.1/edcb.sh";
-  const script = await (await fetch(url)).text();
+  if (Deno.args[0] === "upgrade") {
+    console.info(
+      "[edcb] use `deno install -f -A https://deno.land/x/edcb/cli.ts`",
+    );
+    console.error(red("[edcb] CLI does not yet support `upgrade`"));
+    Deno.exit(1);
+  }
 
-  console.log(cyan("[edcb] installing"));
-  const file = await Deno.makeTempFile();
-  await Deno.writeTextFile(file, script);
+  // NOTE: Infer version of `edcb.sh` from module URL.
+  const thisUrl = import.meta.url;
+  if (!thisUrl.match(/\/cli\.ts$/)) {
+    console.error(red("[edcb] was loaded from unknown source"));
+    Deno.exit(1);
+  }
+  const rootUrl = thisUrl.substr(0, thisUrl.length - "cli.ts".length);
+  const scriptUrl = rootUrl + "edcb.sh";
 
-  console.log(cyan("[edcb] running"));
+  // NOTE: Since we build the bash script URL from the module URL,
+  // it can either be 'file' or 'http' / 'https' protocols.
+  // For example, when running `deno run -A cli.ts` in this project,
+  // the `edcb.sh` file will be used directly as a file. If someone
+  // runs `deno run -A http://deno.land/x/edcb@1.2.3/cli.ts`,
+  // `https://deno.land/x/edcb@1.2.3/edcb.ts` will be downloaded.
+  const { protocol, pathname } = new URL(scriptUrl);
+  let file = pathname;
+
+  // File must be downloaded from URL.
+  if (protocol !== "file:") {
+    console.log(cyan("[edcb] downloading"));
+    const script = await (await fetch(scriptUrl)).text();
+
+    console.log(cyan("[edcb] installing"));
+    file = (await Deno.makeTempFile()) + ".edcb.sh";
+    await Deno.writeTextFile(file, script);
+  }
+
+  console.log(cyan("[edcb] running: " + file));
   const process = Deno.run({
     cmd: ["bash", file, ...Deno.args],
   });
@@ -27,5 +55,6 @@ if (import.meta.main) {
     console.log(green("[edcb] succeeded"));
   } else {
     console.error(red("[edcb] failed"));
+    Deno.exit(1);
   }
 }
