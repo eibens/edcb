4dd4846df4f74da41779213deddb55b4fe6e833a
eibens <l.eibensteiner@gmail.com>
2021-09-26 13:58:09 +0200

    feat: invoke dev.ts script if present (fixes #10)

--- cli.ts
+++ cli.ts
@@ -1,6 +1,17 @@
 import { check, CheckOptions, ServeOptions } from "./mod.ts";
 import { serve } from "./mod.ts";
 
+if (import.meta.main) {
+  const devScript = "dev.ts";
+  if (fileExists(devScript)) {
+    // When a dev.ts script exists, run it.
+    // This prevents version edcb version mismatch.
+    await runScript(devScript, Deno.args);
+  } else {
+    await cli();
+  }
+}
+
 export type CliOptions = {
   serve: Partial<ServeOptions>;
   check: Partial<CheckOptions>;
@@ -19,6 +30,23 @@ export async function cli(options: Partial<CliOptions> = {}) {
   return await command({ args });
 }
 
-if (import.meta.main) {
-  await cli();
+async function runScript(file: string, args: string[]) {
+  const p = Deno.run({
+    cmd: ["deno", "run", "-A", "--unstable", file, ...args],
+  });
+  const status = await p.status();
+  p.close();
+  Deno.exit(status.code);
+}
+
+async function fileExists(file: string) {
+  try {
+    await Deno.lstat(file);
+    return true;
+  } catch (error) {
+    if (!(error instanceof Deno.errors.NotFound)) {
+      throw error;
+    }
+  }
+  return false;
 }
