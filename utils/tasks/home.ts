d8844d66c6e929b39b676c7831aa9f221c845f59
eibens <l.eibensteiner@gmail.com>
2021-11-12 15:27:49 +0100

    refactor: simplify flags parsing (fixes #38)

--- utils/tasks/home.ts
+++ utils/tasks/home.ts
@@ -1,6 +1,6 @@
-import { version } from "../../version.ts";
-import { parseFlags } from "../flags.ts";
+import { parse } from "../../deps/flags.ts";
 import { home as help } from "../help.ts";
+import { version } from "../../version.ts";
 
 export type HomeOptions = {
   help: boolean;
@@ -10,15 +10,18 @@ export type HomeOptions = {
 function parseOptions(
   options: Partial<HomeOptions & { args: string[] }>,
 ): HomeOptions {
-  return parseFlags(options.args || Deno.args, {
+  const flags = parse(options.args || Deno.args, {
     boolean: ["version", "help"],
     string: [],
-    alias: { help: "h", version: "v" },
-    default: {
-      help: false,
-      version: false,
+    alias: {
+      help: "h",
+      version: "v",
     },
   });
+  return {
+    help: flags.help || false,
+    version: flags.version || false,
+  };
 }
 
 export function home(
