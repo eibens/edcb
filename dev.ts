b67e2bd90c1b8da95b7a028f3968f4b90bf050ee
eibens <l.eibensteiner@gmail.com>
2021-11-12 20:36:59 +0100

    chore: remove unnecessary comment

--- dev.ts
+++ dev.ts
@@ -3,12 +3,10 @@ import { cli } from "./cli.ts";
 if (import.meta.main) {
   await cli({
     check: {
-      // These directories should not be linted or formatted.
       ignore: "docs",
     },
     serve: {
       reload: true,
-      webRoot: "docs",
       bundles: [{
         source: "docs/example_script.ts",
         target: "example_script.js",
